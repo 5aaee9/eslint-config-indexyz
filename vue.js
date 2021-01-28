5017556400b13afe0ec422228b01490380087138
Indexyz <jiduye@gmail.com>
2021-01-28 19:34:56 +0800

    release: 1.0.2

--- vue.js
+++ vue.js
@@ -4,10 +4,10 @@ const rules = require('./typescript')
 
 rules.parser = 'vue-eslint-parser'
 rules.parserOptions.parser = '@typescript-eslint/parser'
-rules.extends = [ ...ts.extends, 'plugin:vue/recommended' ]
-rules.plugins = [ ...ts.plugins, 'vue' ]
+rules.extends = [ ...rules.extends, 'plugin:vue/recommended' ]
+rules.plugins = [ ...rules.plugins, 'vue' ]
 rules.rules = {
-    ...ts.rules,
+    ...rules.rules,
     ...require('./rules/vue'),
 }
 
